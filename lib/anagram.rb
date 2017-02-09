5a84d1b5fa2a8e6f9e614a0343122d32ca0abeeb
Your Name <you@example.com>
2017-02-09 00:19:00 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+   attr_accessor :match
+    def initialize(word)
+      @word=word
+    end
+
+      def match(words_list)
+       words_list.find_all do|word|
+          word.split('').sort==@word.split('').sort
+          
+        end
+     #binding.pry
+      end
+end
